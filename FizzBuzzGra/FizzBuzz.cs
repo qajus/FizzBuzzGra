9678ff3fdb4fbaaef1007088f17620d02ab06ce8
Magdalena Skalik <magdalena.skalik@gmail.com>
2021-12-14 23:18:30 +0100

    update project

---
+++ FizzBuzzGra/FizzBuzz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGra
+{
+    public class FizzBuzz
+    {
+        public string GetFizzOrBuzz(int number)
+        {
+            
+            if(number % 3 ==0 && number % 5 != 0)
+            {
+                return "Fizz";
+            }
+            if (number % 5 == 0 && number % 3 != 0)
+            {
+                return "Buzz";
+            }
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            return number.ToString();
+        }
+    }
+}
--- FizzBuzzGra/Program.cs
+++ FizzBuzzGra/Program.cs
@@ -6,7 +6,37 @@ namespace FizzBuzzGra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            var result = new FizzBuzz();
+            while(true)
+            {
+                Console.WriteLine("Podaj liczbe całkowitą");
+                var number = GetNumber();
+                Console.WriteLine(result.GetFizzOrBuzz(number));
+                Console.WriteLine("Jezeli chcesz skończyc wybierze t");
+                
+            }
+            
+
+            Console.ReadLine();
+        }
+
+        private static int GetNumber()
+        {
+            while(true)
+            {
+                var value = Console.ReadLine();
+                if(value.ToUpper()=="T" )
+                {
+                    Environment.Exit(0);
+                }
+                if(!int.TryParse(value, out int number))
+                {
+                    Console.WriteLine("Podany ciąg znaków nie jest liczba całkowitą. Podaj prosze liczbę");
+                    continue;
+                }
+                return number;
+            }
         }
     }
 }
